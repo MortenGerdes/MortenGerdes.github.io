fd2f7c02654377eacf3e5744673680e36e1c4242
Morten <mortengerdes7000@gmail.com>
2019-12-22 13:52:22 +0100

    First commit

---
+++ data.js
@@ -0,0 +1,246 @@
+category_names =
+[
+    "template",
+    "player",
+    "storymode"
+];
+
+data = `
+
+#template:
+    <A> @mood@ @storytype@ where you follow @player:cangroup@ who( , both )(is,are) (<a> , )@character_description@ @character@, on their [journey, travel, trip, way] towards [glory, greatness, legendaryness]
+	<A> @mood@ @storytype@ featuring the one and only @character_description@ @player@ @goal_prefix@ @goal@
+	<A> steamy love-triangle involving @player@ @character_description_post@, @player@ @character_description_post@ and @player@ @character_description_post@. 
+	<A> @storytype@ where the @character_description@ [protagonist, hero, villian, cutie] @player@ @goal_prefix@ convince @player@ to become their secret lover in a @mood@ @storytype@ to save the [world, universe, galaxy, planet, neighbourhood, city, town]
+	Anything where @player:cangroup@ (is,are) cool
+	<A> @storytype@ about a @character_description@ @character@ who just [love, feed on] watching @player@ sleep
+	Mineplex [great, interesting, fun, popular, not bad, decent, breathtaking] again
+	<A> @mood@ @storytype@ where you play as the @character_description@ @character@ with @player@ as your @character_description@ assistant, in a quest for [justice, freedom, love, brotherhood]
+#end
+
+#player:
+	Morten
+	Savannah
+	Necrai
+	Chiss
+	Spu
+	Baekin
+	Dctr
+	Gabby
+	Josie
+	Sterling
+	Aaron
+	Blondebug
+	Alex
+	justMEthewolf
+	Relyh
+	Wanderer
+	Taylor
+	Yurix
+	Kyle
+	Andrew
+#end
+
+#character:
+	alien
+	monster
+	wizard
+	witch
+	adventurer
+	assassin
+	sailor
+	chef
+	robot
+	ghost
+	human
+	skeleton
+	[duck, goose, penguin, fish, whale, shark]
+	[puppy, dog, kitten, cat, mammoth, dinosaur, dragon]
+	[bee, ant, fly, mosquito]
+	snowman
+	scientist
+	programmer
+	student
+	youtuber
+	influencer
+	celebrity
+	president
+	politician
+	[businessman, businesswoman]
+	emperor
+	god
+	ninja
+	technophobe
+	vampire
+	hacker
+	zombie
+	action figure
+	geometric shape
+	thief
+	pirate
+	artificial intelligence
+	[prince, princess, king, queen]
+	[grandmother, grandfather]
+	[troll, goblin, orc, dwarf]
+	knight
+	farmer
+	astronaut
+	pilot
+	chocolatier
+	archeologist
+	doctor
+	police officer
+	detective
+	pyromaniac
+	gamer
+	tax collector
+	plumber
+	electrician
+	mechanic
+#end
+
+#character_description:
+	tiny
+	gigantic
+	lonely
+	time-travelling
+	evil
+	easily-frightened
+	extremely [attractive, short, tall, intimidating, clumsy]
+	good-looking
+	courageous
+	anxious
+	forgetful
+	famous
+	shy
+	scary-looking
+	one-eyed
+	bored
+	ambitious
+	power-hungry
+	two-headed
+	wise
+	elderly
+	greedy
+	invisible
+	magical
+	blind
+	peace-loving
+	polite
+	worried
+	rude
+	genetically [modified, enhanced]
+	grumpy
+	charming
+	energetic
+	imaginary
+	[unfriendly, friendly]
+	nervous
+	optimistic
+	pessimistic
+	undead
+	quirky
+	sarcastic
+	well-dressed
+	lazy
+	talkative
+	royal
+	cunning
+	playful
+	penniless
+	wealthy
+	impatient
+	wind-up
+	law-[abiding,breaking]
+	over-enthusiastic
+#end
+
+#character_description_post:
+	from [the future, another planet, another dimension, an alternate reality, a parallel universe]
+	with no [friends, money, morals, soul]
+	who loves [kittens, puppies, rainbows, cars, spaceships, animals, flowers, long walks, writing, baking, gardening, fishing, fire]
+	who (is,are) passionate about [recycling, model trains, education, animal-rights, stamp-collecting, arcade games, board games, tea]
+	with [a tragic backstory, an adorable pet, a secret superpower, a secret identity]
+	who (can,can) [fly, bend time, turn invisible, teleport, summon spirits]
+	with a debilitating fear of [heights, ghosts, flying, water, being alone, people, blood, small spaces, crowds, spiders]
+	with a tendency to [overreact, faint at the sight of blood, spontaneously combust, over-share]
+	who (loathes,loathe) [violence, swimming, daylight, robots, humans, aliens]
+	with a controversial opinion on [politics, pizza toppings, education, robots, science]
+	who (is,are) addicted to [chocolate, pizza, fast-food, cartoons, danger, tea, coffee]
+#end
+
+#goal_prefix:
+	[trying, attempting] to
+	who [desperately,] (wants,want) to
+	who will stop at nothing to
+	who (needs,need) to
+#end
+
+#goal:
+	become the @character_description@ [ruler, god, champions] of @mpgames@
+	become the @character_description@ lover of @character_description@ @player:cangroup@ @character_description_post@
+#end
+
+#mpgames:
+	Bawk Bawk Battles
+	Champions
+	Clans
+	Nano Games
+	Cake Wars
+	Survival Games
+	Bridges
+	Micro Battles
+	Mixed Arcade
+	Dragons
+	Super Paintball
+	Squids
+	Sneaky Assassins
+	Wither Assault
+	Block hunt
+	Castle Siege
+	Super Smash Mobs
+#end
+
+#storytype:
+	fairy tale
+	myth
+	story
+	tale
+	legend
+	animation
+	3D animation
+	blockbuster movie
+	stop-motion film
+#end
+
+#mood:
+	frustrating
+	challenging
+	rage-inducing
+	stressful
+	relaxing
+	meditative
+	calming
+	thought-provoking
+	addictive
+	[humorous, amusing]
+	whimsical
+	[scary, frightening, terrifying]
+	innovative
+	inspiring
+	groovey
+	artsy
+	upbeat
+	violent
+	peaceful
+	dreamy
+	stylish
+	short-and-sweet
+	slow-paced
+	intense
+	nostalgic
+	[strange, quirky, unusual] little
+	satirical
+#end
+
+`
---
+++ generator.js
@@ -0,0 +1,265 @@
+var isCharacterInGroup = false
+
+function generate() 
+{
+    document.getElementById("content").innerHTML = fillTemplate(pickRandomFromList(getCategoryOptionsArray("template"))) + ".";
+
+    fillTemplate(pickRandomFromList(getCategoryOptionsArray("template")))
+}
+
+function fillTemplate(template)
+{
+    if(template.includes("@"))
+    {
+        let command = getTextBetweenTags(template, "@", "@")
+        let keyword = command.split(":")[0]
+        let replace = "NOTHING TO REPLACE"
+        let modifiers = [];
+
+		if (command.includes(':')) {
+			modifiers = command.split(':')[1].split(',');
+		}
+
+        switch(keyword) 
+        {
+            case "player":
+                replace = generatePlayer(modifiers)
+                break;
+            case "storytype":
+                replace = generateStorymode(modifiers)
+                break;
+            case "character":
+                replace = generateCharacter(modifiers)
+                break;
+            case "character_description":
+                replace = generateCharacterDesc(modifiers)
+                break;
+            case "character_description_post":
+                replace = generateCharacterDescPost(modifiers)
+                break;
+            case "mpgames":
+                replace = generateMPGames(modifiers)
+                break;
+            case "goal_prefix":
+                replace = generateGoalPrefix(modifiers)
+                break;
+            case "goal":
+                replace = generateGoal(modifiers)
+                break;
+            case "mood":
+                replace = generateMood(modifiers)
+                break;
+        }
+
+        template = replaceBetweenTags(template, replace, "@", "@")
+        return fillTemplate(template)
+    }
+    if(template.includes("\t"))
+    {
+        template = template.split("\t").join("")
+    }
+    template = parseBrackets(template)
+    template = parseOption(template)
+
+    if(template.includes("<"))
+    {
+        template = parseArticle(template)
+        return fillTemplate(template)
+    }
+
+    return template
+}
+
+function generatePlayer(modifiers)
+{
+    let result
+    if(modifiers.includes("cangroup") && randomChance(0.2))
+    {
+        result = "@player@ and @player@"
+        isCharacterInGroup = true
+    }
+    else
+    {
+        result = pickRandomFromList(getCategoryOptionsArray("player"))
+    }
+
+    return result
+}
+
+function generateCharacter(modifiers)
+{
+    let result
+    result = isCharacterInGroup ? pluralize(pickRandomFromList(getCategoryOptionsArray("character"))) : pickRandomFromList(getCategoryOptionsArray("character"))
+
+    return result
+}
+
+function generateCharacterDesc(modifiers)
+{
+    let result
+    result = pickRandomFromList(getCategoryOptionsArray("character_description"))
+
+    return result
+}
+
+function generateCharacterDescPost(modifiers)
+{
+    let result
+    result = pickRandomFromList(getCategoryOptionsArray("character_description_post"))
+
+    return result
+}
+
+function generateStorymode(modifiers)
+{
+    let result
+    result = pickRandomFromList(getCategoryOptionsArray("storytype"))
+
+    return result
+}
+
+function generateGoalPrefix(modifiers)
+{
+    let result
+    result = pickRandomFromList(getCategoryOptionsArray("goal_prefix"))
+
+    return result
+}
+
+function generateGoal(modifiers)
+{
+    let result
+    result = pickRandomFromList(getCategoryOptionsArray("goal"))
+
+    return result
+}
+
+function generateMPGames(modifiers)
+{
+    let result
+    result = pickRandomFromList(getCategoryOptionsArray("mpgames"))
+
+    return result
+}
+
+function generateMood(modifiers)
+{
+    let result
+    result = pickRandomFromList(getCategoryOptionsArray("mood"))
+
+    return result
+}
+
+function getCategoryOptionsArray(name)
+{
+    let toReturn = getTextBetweenTags(data, "#" + name + ":\n", "#end").split("\n")
+    return toReturn.slice(0, toReturn.length-1)
+}
+
+function getTextBetweenTags(text, startTag, endTag) 
+{
+	return text.split(startTag)[1].split(endTag)[0];
+}
+
+function replaceBetweenTags(text, replace, startTag, endTag)
+{
+    let textBetween = getTextBetweenTags(text, startTag, endTag)
+    let startRemoved = text.replace(startTag, "")
+    let endRemoved = startRemoved.replace(endTag, "")
+
+    return endRemoved.replace(textBetween, replace)
+}
+
+function parseBrackets(text)
+{
+    let replacementText = text
+
+    while (replacementText.includes("["))
+    {
+        let wordsWithin = getTextBetweenTags(replacementText, "[", "]").split(", ")
+        replacementText = replaceBetweenTags(replacementText, pickRandomFromList(wordsWithin), "[", "]")
+    }
+
+    return replacementText
+}
+
+function parseArticle(text)
+{
+    let replacementText = text
+    let isACap = (text.charAt(text.indexOf("<") + 1) == text.charAt(text.indexOf("<") + 1).toUpperCase())
+
+    if(text.charAt(text.indexOf("<") + 4).match(/[aeiou]/i))
+    {
+        replacementText = replaceBetweenTags(replacementText, isACap ? "An" : "an", "<", ">") 
+    }
+    else
+    {
+        replacementText = replaceBetweenTags(replacementText, isACap ? "A" : "a", "<", ">") 
+    }
+
+    return replacementText
+}
+
+function parseOption(text)
+{
+    let replacementText = text
+
+    while (replacementText.includes("("))
+    {
+        let wordsWithin = getTextBetweenTags(replacementText, "(", ")").split(",")
+        replacementText = replaceBetweenTags(replacementText, isCharacterInGroup ? wordsWithin[1] : wordsWithin[0], "(", ")")
+    }
+    isCharacterInGroup = false
+    return replacementText
+}
+
+function pluralize(word) {
+	// exceptions:
+	switch (word) {
+	case 'goose':
+		return 'geese';
+	case 'fish':
+		return 'fish';
+	case 'human':
+		return 'humans';
+	case 'fish':
+		return word;
+	case 'thief':
+		return 'thieves';
+	default:
+		// general rules:
+		if (word.substr(-2) == 'ey') {
+			return word + 's';
+		}
+		if (word.substr(-1) == 'y') {
+			return word.substring(0, word.length-1) + 'ies';
+		}
+		if (word.substr(-1) == 'h') {
+			return word + 'es';
+		}
+		if (word.substr(-3) == 'man') {
+			return word.substring(0, word.length-3) + 'men';
+		}
+		if (word.substr(-5) == 'child') {
+			return word + 'ren';
+		}
+
+		return word + 's';
+	}
+}
+
+function cleanOutput(text)
+{
+    // Clean taps etc.
+}
+
+function randomChance(probability) 
+{
+	return Math.random() < probability;
+}
+
+function pickRandomFromList(list)
+{
+        let randomIndex = Math.floor(Math.random() * list.length); 
+        return list[randomIndex];
+}
